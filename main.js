2cb4f91b82ce66a23f5652247c986b36968e4c58
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-04-23 19:46:33 +0900

    sec01-18

--- main.js
+++ main.js
@@ -170,3 +170,31 @@ for( num of nums ){
 for(let i = 0; i < 10; i ++){
     console.log(i);
 }
+
+//switch (ifのほうが見やすい)
+const data = 1;
+
+switch(data){
+    case 1:
+        console.log('1です');
+        break
+    case 2:
+        console.log('2です');
+        break
+    case 3:
+        console.log('3です');
+        break
+    default:
+        console.log('1-3ではありません');
+        break
+}
+
+if(data === 1){
+    console.log('1です');
+}
+if(data === 2){
+    console.log('2です');
+}
+if(data === 3){
+    console.log('3です');
+}
