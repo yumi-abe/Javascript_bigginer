c90cb07c26be6c0701f139772968d3b2e5d88dd4
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-05-05 20:42:14 +0900

    modal

--- 01/dom/modal/main.js
+++ 01/dom/modal/main.js
@@ -7,14 +7,15 @@ const backGround = document.getElementById('modal__background');
 
 show.addEventListener('click', () => {
     modal.classList.add('is-show');
-    // backGround.classList.add('is-show');
+    backGround.classList.add('is-show');
 })
 
 close.addEventListener('click', () => {
     modal.classList.remove('is-show');
-    // backGround.classList.remove('is-show');
+    backGround.classList.remove('is-show');
 })
 
-backGround.addEventListener('click',() => {
+backGround.addEventListener('click', () => {
     close.click();
 })
+
