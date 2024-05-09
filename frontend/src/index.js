91b792148af907850d8c7ccee1d439ad315e456a
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-05-09 11:05:55 +0900

    sec06

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
 import './style.scss';
+const moment = require('moment');
 
+console.log(moment().format());
+console.log(moment().year());
+console.log(moment().format('YYYY-MM-DD'));
 console.log('test');
 
-window.addEventListener('load', () =>{
-    alert('babelテストです');
-})
+// window.addEventListener('load', () =>{
+//     alert('babelテストです');
+// })
